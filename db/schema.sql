1d99c3a37910b08458e6232e722cd39e36628da4
MaryEtak <marykatewhite@gmail.com>
2020-02-15 18:15:12 -0500

    fixed my schema, wrote some readme, tryna do a gif

--- db/schema.sql
+++ db/schema.sql
@@ -24,5 +24,5 @@ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT NOT NULL,
-manager_id INT NULL,
-);
+manager_id INT NULL
+)
